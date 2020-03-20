1f020bc167f5f907f765ae3a73d3d77b3919437c
Li, Jiajia <jiajia.li@intel.com>
2020-03-20 15:43:04 +0800

    return pegasus datasource when the datasource is parquet

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSource.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/DataSource.scala
@@ -634,6 +634,7 @@ object DataSource extends Logging {
         "org.apache.spark.sql.hive.orc.OrcFileFormat"
       case "com.databricks.spark.avro" if conf.replaceDatabricksSparkAvroEnabled =>
         "org.apache.spark.sql.avro.AvroFileFormat"
+      case name if name.equalsIgnoreCase("parquet") => "pegasus"
       case name => name
     }
     val provider2 = s"$provider1.DefaultSource"
